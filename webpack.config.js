1b11f9e6bd32e48b75e34cc438a2469a352aeaf6
Luy Tran <octopitus@gmail.com>
2016-06-28 09:31:19 +0700

    Change NODE_ENV to browser

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
 		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.DefinePlugin({
 		  'process.env': {
-		    'NODE_ENV': JSON.stringify('production')
+		    'NODE_ENV': JSON.stringify('browser')
 		  }
 		}),
 		new webpack.optimize.UglifyJsPlugin({
